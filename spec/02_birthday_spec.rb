a8fec1d80e95a6d1e7fd6f76a79e6525f4b4c50a
angelo milien <angelomilienjoseph@gmail.com>
2021-03-02 14:20:52 -0500

    done

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -13,7 +13,6 @@ describe "#happy_birthday" do
   end
 end
 
-## BONUS ##
 ## Change "xit" to "it" in the test block to enable
 describe "#age_appropriate_birthday" do
   it "only prints the birthday greeting if the birthday kid 12 or younger" do
